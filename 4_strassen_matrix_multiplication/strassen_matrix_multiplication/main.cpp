8e00897b9fecab36a6d3a480aefa0a9561126a6e
amir hosein moradi <rahaansharif01@gmail.com>
2026-01-05 17:46:01 +0330

    update.

--- 4_strassen_matrix_multiplication/strassen_matrix_multiplication/main.cpp
+++ 4_strassen_matrix_multiplication/strassen_matrix_multiplication/main.cpp
@@ -70,21 +70,59 @@ int* zero_padding(int array[], int length1, int length2, int length)
     return new_array;
 }
 
-int* strassen_matrix_multiplication(int* array, int length1, int length2)
+int* partitioning(int* array1, int length1, int* array2, int length2)
 {
-    int tmp_length=-1;
-    if(length1>length2)
+    if(length1<2 || length2<2)
     {
-        tmp_length=length1;
+        return NULL;
+    }
+    else if(length==2)
+    {
+
     }
     else
     {
-        tmp_length=length2;
+
+    }
+}
+
+int* strassen_matrix_multiplication(
+        int* array1, int length11, int length12,
+        int*array2, int length21, int length22)
+{
+    int tmp_length1=-1, tmp_length2=-1, tmp_length=-1;
+    if(length11>length12)
+    {
+        tmp_length1=length11;
+    }
+    else
+    {
+        tmp_length1=length12;
     }
 
-    int tmp_length2=get_near_upper_power2(tmp_length);
+    if(length21>length22)
+    {
+        tmp_length2=length21;
+    }
+    else
+    {
+        tmp_length2=length22;
+    }
 
-    int* array2=zero_padding(array, length1, length2, tmp_length2);
+    if(tmp_length1>tmp_length2)
+    {
+        tmp_length=tmp_length1;
+    }
+    else
+    {
+        tmp_length=tmp_length2;
+    }
+
+    tmp_length=get_near_upper_power2(tmp_length);
+
+    array1=zero_padding(array1, length11, length12, tmp_length);
+    array2=zero_padding(array2, length21, length22, tmp_length);
+
 
 
 
