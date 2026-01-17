425b2776bc951d86704fce41a2434fd1510b2964
amir hosein moradi <rahaansharif01@gmail.com>
2026-01-18 01:15:56 +0330

    finished.

--- 6_fibonacci_nubmers/fibonacci_numbers/main.cpp
+++ 6_fibonacci_nubmers/fibonacci_numbers/main.cpp
@@ -1,8 +1,39 @@
-#include <QCoreApplication>
+#include <iostream>
+using namespace std;
+#define lli long long int
 
-int main(int argc, char *argv[])
+lli fibonacci_func(lli n)
 {
-    QCoreApplication a(argc, argv);
+    lli a1=0;
+    lli a2=1;
+    if(n==0)
+    {
+        return a1;
+    }
+    else if(n==1)
+    {
+        return a2;
+    }
+    else
+    {
+        lli i=2;
+        while(i<=n)
+        {
+            lli tmp=a1+a2;
+            a1=a2;
+            a2=tmp;
+            i++;
+        }
+        return  a2;
+    }
+}
+int main(void)
+{
+    for(int i=0; i<10; i++)
+    {
+        cout<<i<<"\t"<<fibonacci_func(i)<<"\n";
+    }
 
-    return a.exec();
+
+    return 0;
 }
