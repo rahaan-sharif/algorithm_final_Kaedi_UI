b7690bca650732144d6ab61c1368a9142578cd4b
amir hosein moradi <rahaansharif01@gmail.com>
2026-02-04 21:00:31 +0330

    update.

--- 9_matrix_chain_multiplication/matrix_chain_multiplication/main.cpp
+++ 9_matrix_chain_multiplication/matrix_chain_multiplication/main.cpp
@@ -16,6 +16,30 @@ public:
         array=array_in;
     }
 
+    d* mul(d*& mat1, d*& mat2, int l1, int l2, int l3)
+    {
+        int* output_arr=new int[l1*l3];
+        for(int i=0; i<l1*l3; i++)
+        {
+            output_arr[i]=0;
+        }
+
+        for(int i=0; i<l1; i++)
+        {
+            for(int j=0; j<l2; j++)
+            {
+                for(int k=0; k<l3; k++)
+                {
+                    output_arr[(i*l3)+k] += mat1->array[(i*l2)+j] * mat2->array[(j*l3)+k];
+                }
+            }
+        }
+
+
+        d* output=new d(l1, l3, output_arr);
+        return output;
+    }
+
     void show()
     {
         cout<<"show d:"<<endl;
@@ -92,16 +116,29 @@ public:
 
 };
 
+void set_mat(int*& arr, int l1, int l2)
+{
+    int tmp=(l1*l2)+11;
+    for(int i=0; i<l1*l2; i++)
+    {
+        arr[i]=((((i*17)+((i*37)*43)/(19))<<2)*23*tmp+tmp+7)%10;
+        tmp=(tmp*29)%97;
+    }
+}
+
 int main(void)
 {
-    int* array=new int[12];
-    for(int i=0; i<12; i++)
-    {
-        array[i]=i;
-    }
+    int* arr1=new int[3*4];
+    int* arr2=new int[4*5];
+    set_mat(arr1, 3, 4);
+    set_mat(arr2, 4, 5);
 
-    d tmp=d(3, 4, array);
-    tmp.show();
+    d* tmp1=new d(3, 4, arr1);
+    d* tmp2=new d(4, 5, arr2);
+    tmp1->show();
+    tmp2->show();
+
+    element** table=NULL;
 
 
 
