650baf53ecfc012c10d8031940235a8c89cffeee
amir hosein moradi <rahaansharif01@gmail.com>
2026-02-05 16:27:35 +0330

    main function is defined but is not completed. linked list must be changed to binary search tree.

--- 9_matrix_chain_multiplication/matrix_chain_multiplication/main.cpp
+++ 9_matrix_chain_multiplication/matrix_chain_multiplication/main.cpp
@@ -1,77 +1,102 @@
 #include <iostream>
 using namespace std;
+#define mat_count 6
 
 class d
 {
 private:
-    int length;
-    int height;
+    int l1;
+    int l2;
     int* array;
 
 public:
-    d(int length_in=-1, int height_in=-1, int* array_in=NULL)
+    d(int l1_in=-1, int l2_in=-1, int* array_in=NULL)
     {
-        length=length_in;
-        height=height_in;
+        l1=l1_in;
+        l2=l2_in;
         array=array_in;
     }
 
-    d* mul(d*& mat1, d*& mat2, int l1, int l2, int l3)
+    d* mul(d*& mat1, d*& mat2)
     {
-        int* output_arr=new int[l1*l3];
-        for(int i=0; i<l1*l3; i++)
+        if(mat1->l2!=mat2->l1)
         {
-            output_arr[i]=0;
+            cout<<"error! matrices can't be multiplied to each other.\n";
+            return NULL;
         }
-
-        for(int i=0; i<l1; i++)
+        else
         {
-            for(int j=0; j<l2; j++)
+            int ll1=mat1->l1;
+            int ll2=mat1->l2;
+            int ll3=mat2->l2;
+
+            int* output_arr=new int[ll1*ll3];
+            for(int i=0; i<ll1*ll3; i++)
             {
-                for(int k=0; k<l3; k++)
+                output_arr[i]=0;
+            }
+
+            for(int i=0; i<ll1; i++)
+            {
+                for(int j=0; j<ll2; j++)
                 {
-                    output_arr[(i*l3)+k] += mat1->array[(i*l2)+j] * mat2->array[(j*l3)+k];
+                    for(int k=0; k<ll3; k++)
+                    {
+                        output_arr[(i*ll3)+k] += mat1->array[(i*ll2)+j] * mat2->array[(j*ll3)+k];
+                    }
                 }
             }
+
+
+            d* output=new d(ll1, ll3, output_arr);
+            return output;
         }
-
-
-        d* output=new d(l1, l3, output_arr);
-        return output;
     }
 
-    void show()
+    void show(d* start)
     {
         cout<<"show d:"<<endl;
-        for(int i=0; i<length; i++)
+        for(int i=0; i<start->l1; i++)
         {
-            for(int j=0; j<height; j++)
+            for(int j=0; j<start->l2; j++)
             {
-                cout<<array[(i*height)+j]<<"\t";
+                cout<<start->array[(i*start->l2)+j]<<"\t";
             }
             cout<<endl;
         }
         cout<<endl<<endl;
+    }
+
+    int get_l1(d* start)
+    {
+        return start->l1;
+    }
+
+    int get_l2(d* start)
+    {
+        return start->l2;
     }
 };
 
 class linked_list
 {
 private:
-    d* node;
+    int l1, l2;
     linked_list* next;
 
 public:
-    linked_list(d* node_in=NULL, linked_list* next_in=NULL)
+    linked_list(int l1_in=0, int l2_in=0)
     {
-        node=node_in;
-        next=next_in;
+        l1=l1_in;
+        l2=l2_in;
+        next=NULL;
     }
-    void make_list(linked_list*& start, d* node_in)
+
+    void make_list(linked_list*& start, int l1_in, int l2_in)
     {
         if(start==NULL)
         {
-            start=new linked_list(node_in, NULL);
+            start=new linked_list(l1_in, l2_in);
             return;
         }
         else
@@ -81,17 +106,38 @@ public:
             {
                 tmp_ll=tmp_ll->next;
             }
-            tmp_ll->next=new linked_list(node_in, NULL);
+            tmp_ll->next=new linked_list(l1_in, l2_in);
             return;
         }
     }
+
+    void concat_2_lists(linked_list*& start1, linked_list*& start2)
+    {
+        if(start2==NULL)
+        {
+            return;
+        }
+        else if(start1==NULL)
+        {
+            start1=start2;
+        }
+        else
+        {
+            linked_list* tmp_ll=start1;
+            while(tmp_ll->next!=NULL)
+            {
+                tmp_ll=tmp_ll->next;
+            }
+            tmp_ll->next=start2;
+        }
+    }
+
     void show(linked_list* start)
     {
         while(start!=NULL)
         {
-            start->node->show();
+            cout<<"("<<start->l1<<", "<<start->l2<<") -> ";
             start=start->next;
-            cout<<endl;
         }
     }
 };
@@ -99,25 +145,39 @@ public:
 class element
 {
 private:
+    d* matrix;
     linked_list* path;
     int value;
 
 public:
-    element(linked_list* path_in, int value_in)
+    element()
     {
-        path=path_in;
-        value=value_in;
+        path=NULL;
+        value=0;
+        matrix=NULL;
+    }
+
+    void set_element(element*& start1, element*& start2, element*& start3)
+    {
+        d* tmp_d=tmp_d->mul(start2->matrix, start3->matrix);
+        int tmp_l1=start2->matrix->get_l1(start2->matrix);
+        int tmp_l2=start2->matrix->get_l2(start2->matrix);
+        int tmp_l3=start3->matrix->get_l2(start3->matrix);
+        int tmp_value=tmp_l1*tmp_l2*tmp_l3;
+        linked_list* tmp_path=;
     }
 
     void add_path(element*& start, d* node_in)
     {
-        start->path->make_list(start->path, node_in);
+        start->path->make_list(start->path, node_in);  // باید یک ساختتار درختی باشد. این اشتباه است.
     }
 
 };
 
 void set_mat(int*& arr, int l1, int l2)
 {
+    arr=new int[l1*l2];
+
     int tmp=(l1*l2)+11;
     for(int i=0; i<l1*l2; i++)
     {
@@ -126,17 +186,36 @@ void set_mat(int*& arr, int l1, int l2)
     }
 }
 
+void mcm(int**& arr, int* l, int count)      //matrix chain multiplication
+{
+    d** matrices=new d*[count];
+    for(int i=0; i<count; i++)
+    {
+        matrices[i]=new d(l[i], l[i+1], arr[i]);
+    }
+
+
+
+}
+
 int main(void)
 {
-    int* arr1=new int[3*4];
-    int* arr2=new int[4*5];
-    set_mat(arr1, 3, 4);
-    set_mat(arr2, 4, 5);
+    int* l=new int[mat_count+1];
+    l[0]= 5;
+    l[1]= 2;
+    l[2]= 3;
+    l[3]= 4;
+    l[4]= 6;
+    l[5]= 7;
+    l[6]= 8;
 
-    d* tmp1=new d(3, 4, arr1);
-    d* tmp2=new d(4, 5, arr2);
-    tmp1->show();
-    tmp2->show();
+    int** arr=new int*[mat_count];
+    for(int i=0; i<mat_count; i++)
+    {
+        set_mat(arr[i], l[i], l[i+1]);
+    }
+
+    mcm(arr, l, mat_count);
 
     element** table=NULL;
 
